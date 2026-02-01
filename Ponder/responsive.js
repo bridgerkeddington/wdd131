90e9e00c7414f655ce024bdad02ab2e410bc4a12
bridgerkeddington <brkedding@gmail.com>
2026-01-31 22:38:53 -0700

    Finish responsive menu toggle

---
+++ Ponder/responsive.js
@@ -0,0 +1,10 @@
+// Updated by Codex
+const menuButton = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+function toggleMenu() {
+  menu.classList.toggle('hide');
+  menuButton.classList.toggle('change');
+}
+
+menuButton.addEventListener('click', toggleMenu);
